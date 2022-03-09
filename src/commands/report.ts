f4b9acaaa9f7842753115a063d87359d687e1c49
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-03-09 04:03:50 +0100

    fix: catch DiscordAPIError when run commands

--- src/commands/report.ts
+++ src/commands/report.ts
@@ -1,5 +1,6 @@
 import {SlashCommandBuilder} from "@discordjs/builders";
 import {Command} from "../libs/Command";
+import {DiscordAPIError, MessageEmbed} from "discord.js";
 
 export default new Command({
     commandDMOnly: true,
@@ -7,6 +8,17 @@ export default new Command({
         .setName("report")
         .setDescription("report a message"),
     run: async ({client, interaction}) => {
-        await client.reportManager.handleCommand(interaction);
+        try {
+            await client.reportManager.handleCommand(interaction);
+        } catch (err) {
+            if (err instanceof DiscordAPIError && err.code === 50007) {
+                await interaction.reply({
+                    embeds: [new MessageEmbed().setTitle("Error").setDescription("Your private chat is disabled! Please enable it in Privacy Settings and try again.")],
+                    ephemeral: true
+                })
+            } else {
+                throw(err);
+            }
+        }
     }
 });
